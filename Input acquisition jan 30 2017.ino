17f323b161af54f8743e2ad932a4adaedcacf7cf
LSUPERMAN735 <lsuperman735@outlook.fr>
2018-02-06 15:33:45 +0100

    Update Input acquisition jan 30 2017.ino

--- Input acquisition jan 30 2017.ino
+++ Input acquisition jan 30 2017.ino
@@ -19,3 +19,28 @@ void loop() {
       }
     delay(200);
 }
+----------------------------------------------------------------------
+  //code part b //
+  int etatSwitch=0;
+
+void setup() {
+  pinMode(3, INPUT);
+  digitalWrite(3, LOW);
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (digitalRead(3) == 1)
+  {
+    Serial.println("Le bouton 3 est appuye");
+  }
+
+  if (digitalRead(2) == 0)
+  {
+    Serial.println("Le bouton 2 est appuye");
+  }
+
+delay(200);
+}
